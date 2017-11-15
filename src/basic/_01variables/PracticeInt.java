fde6da30b8a1c612fc0ecff680d59bcc26561a07
elzup <guild0105@gmail.com>
2017-11-15 18:59:28 +0900

    Initialize commit

---
+++ src/basic/_01variables/PracticeInt.java
@@ -0,0 +1,17 @@
+package basic._01variables;
+
+public class PracticeInt {
+  public static void main(String[] args) {
+    int[] k = new int[10];
+    System.out.println(k.length());
+    System.out.println(k.length);
+
+    String s = "";
+    System.out.println(s.length());
+    System.out.println(s.length);
+
+    char[] cs = new char[10];
+    System.out.println(cs.length());
+    System.out.println(cs.length);
+  }
+}
---
+++ src/basic/_01variables/PracticeLong.java
@@ -0,0 +1,6 @@
+package basic._01variables;
+
+public class PracticeLong {
+  public static void main(String[] args) {
+  }
+}
