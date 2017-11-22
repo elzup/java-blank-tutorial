b2f45433b51e57eb7e578e6e7923a564ac19e949
elzup <guild0105@gmail.com>
2017-11-22 19:30:03 +0900

    Add switch test

---
+++ src/_02Statements/StatementSwitchTest.java
@@ -0,0 +1,39 @@
+package _02Statements;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StatementSwitchTest {
+  @Test
+  public void toStr() throws Exception {
+    Assert.assertEquals(StatementSwitch.toStr(1), "One");
+    Assert.assertEquals(StatementSwitch.toStr(2), "Two");
+    Assert.assertEquals(StatementSwitch.toStr(3), "Three");
+    Assert.assertEquals(StatementSwitch.toStr(0), "-");
+    Assert.assertEquals(StatementSwitch.toStr(-1), "-");
+    Assert.assertEquals(StatementSwitch.toStr(100), "-");
+  }
+
+  @Test
+  public void toNumber() throws Exception {
+    Assert.assertEquals(StatementSwitch.toNumber("A"), 100);
+    Assert.assertEquals(StatementSwitch.toNumber("B"), 200);
+    Assert.assertEquals(StatementSwitch.toNumber("C"), 200);
+    Assert.assertEquals(StatementSwitch.toNumber("D"), 300);
+    Assert.assertEquals(StatementSwitch.toNumber("E"), 300);
+    Assert.assertEquals(StatementSwitch.toNumber("hoge"), -1);
+    Assert.assertEquals(StatementSwitch.toNumber("a"), -1);
+  }
+
+  @Test
+  public void toLogical() throws Exception {
+    Assert.assertEquals(StatementSwitch.toLogical(1), 123);
+    Assert.assertEquals(StatementSwitch.toLogical(2), 777);
+    Assert.assertEquals(StatementSwitch.toLogical(3), 999);
+    Assert.assertEquals(StatementSwitch.toLogical(400), -1);
+    Assert.assertEquals(StatementSwitch.toLogical(-1), -1);
+  }
+
+}
